abb9ba1a5692ba29e6c77d2d70f05bceb5ad5f73
fangxin <514063663@qq.com>
2021-08-26 22:17:49 +0800

    项目初始化

---
+++ zjy/src/main/java/com/atstudy/zjy/ZjyApplication.java
@@ -0,0 +1,13 @@
+package com.atstudy.zjy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZjyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZjyApplication.class, args);
+    }
+
+}
